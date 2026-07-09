f0204395aa0be7048fc9737660db23e1bfe3007c
ar9av <ar9avg@gmail.com>
2026-07-08 22:20:41 -0700

    Release 1.17.14

--- prismor/runtime/__init__.py
+++ prismor/runtime/__init__.py
@@ -1,6 +1,6 @@
 """Prismor local session-security utility."""
 
-__version__ = "1.17.13"
+__version__ = "1.17.14"
 
 from prismor.runtime.semantic_guard import SemanticGuard, SemanticRisk
 from prismor.runtime.semantic_guard_v2 import SemanticGuardV2, HybridRisk
